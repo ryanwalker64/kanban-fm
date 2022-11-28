9d81eacf84f37012a040fc3d02abdd6b62d73bbd
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-11-29 09:00:00 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactSwitch from 'react-switch';
+import lightModeLogo from "./assets/logo-dark.svg"
+
+
+function Sidebar(props) {
+  return (
+    <div className="sidebar">
+
+      <div className="sidebar-heading">
+        <img src={lightModeLogo} alt="logo"/>
+      </div>
+
+      <div className="boards-container">
+        <p className="board-counter">ALL BOARDS (3)</p>
+        <ul className="board-list">
+          <li className="board-selector" data-board="active">
+            <img src="" alt="logo"/>
+            <span className="board-selector-name">Platform Launch</span>
+          </li>
+          <li className="board-selector" data-board="create-new-board">
+            <img src="" alt="logo"/>
+            <span className="board-selector-name">+ Create New Board</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="brightness-mode">
+        <img src="" alt="logo"/>
+        
+        <ReactSwitch
+        checked={props.brightnessChecked}
+        onChange={props.toggleBrightness}
+        onColor="#635FC7"
+        offColor='#A8A4FF'
+        uncheckedIcon={false}
+        checkedIcon={false}
+        handleDiameter={14}
+        height={20}
+        width={40}
+        />
+
+        <img src="" alt="logo"/>
+      </div>
+
+      <div className="toggleSidebar">
+        <img src="" alt="logo"/>
+        <span>Hide Sidebar</span>
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
+  const [brightnessChecked, setBrightnessChecked] = useState(true);
+
+  function toggleBrightness(val)  {
+    setBrightnessChecked(val)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        toggleBrightness={toggleBrightness}
+        brightnessChecked={brightnessChecked}
+      />
     </div>
   );
 }
